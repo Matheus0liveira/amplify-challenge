1f4591c25799c274910678f555dfee9bc654ef65
Matheus0liveira <matheusfilho98.ms@gmail.com>
2022-06-16 02:06:51 -0300

    styles: improve

--- src/pages/Editor/index.tsx
+++ src/pages/Editor/index.tsx
@@ -1,13 +1,24 @@
-import { InputRow } from "aws-amplify-react";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Button, InputRow } from "aws-amplify-react";
 import { ChangeEvent, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { useNavigate } from "react-router-dom";
 
 import * as S from "./styles";
 
-export default function Editor() {
+type Thumbnail = {
+  filename: string;
+  file: File;
+  fileUrl: string;
+};
+
+function Editor() {
   const [content, setContent] = useState("");
   const [title, setTitle] = useState("");
+  const [thumbnail, setThumbnail] = useState<Thumbnail>({} as Thumbnail);
+
+  const navigate = useNavigate();
 
   function handleContentChange(text: string) {
     setContent(text);
@@ -15,9 +26,36 @@ export default function Editor() {
   function handleTitleChange(e: ChangeEvent<HTMLInputElement>) {
     setTitle(e.target.value);
   }
+  function handleThumbnailChange(e: ChangeEvent<HTMLInputElement>) {
+    if (!e.target.files?.length) return;
+    const file = e.target.files[0];
+    setThumbnail({
+      file,
+      filename: file.name,
+      fileUrl: URL.createObjectURL(file),
+    });
+  }
+
+  function handlePostCreation() {}
+  function handleGoToHome() {
+    navigate("/");
+  }
 
   return (
     <S.Container>
+      <S.ButtonContainer>
+        <Button onClick={handleGoToHome}>Voltar aos posts</Button>
+        <Button onClick={handlePostCreation}>Salvar post</Button>
+      </S.ButtonContainer>
+      {!!thumbnail.fileUrl && (
+        <img src={thumbnail.fileUrl} alt={`Capa - ${thumbnail.filename}`} />
+      )}
+      <InputRow
+        placeholder="Capa"
+        value={title}
+        onChange={handleThumbnailChange}
+        type="file"
+      />
       <InputRow
         placeholder="Título"
         value={title}
@@ -27,3 +65,5 @@ export default function Editor() {
     </S.Container>
   );
 }
+
+export default withAuthenticator(Editor, { socialProviders: ["google"] });
--- src/pages/Editor/styles.ts
+++ src/pages/Editor/styles.ts
@@ -1,10 +1,29 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  width: 80%;
+  margin: 1rem 10%;
+
   .ql-toolbar.ql-snow:first-of-type {
     display: none;
   }
   .ql-editor {
     height: 80vh;
   }
+
+  img {
+    max-width: 20rem;
+    max-height: 20rem;
+  }
+
+  input {
+    margin-bottom: 0.5rem;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  margin-bottom: 1rem;
 `;
